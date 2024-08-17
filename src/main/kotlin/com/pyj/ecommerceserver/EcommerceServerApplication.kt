7f3f20f3f7017202559aa6bb107fa5db58c45b98
박영준\qdw07 <qdw0719@naver.com>
2024-08-17 13:21:53 +0900

    Initail Commit

---
+++ src/main/kotlin/com/pyj/ecommerceserver/EcommerceServerApplication.kt
@@ -0,0 +1,11 @@
+package com.pyj.ecommerceserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceServerApplication>(*args)
+}
